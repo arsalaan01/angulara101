c84c7b8c0251d8b8203a166ca7c181acc765cb19
arsalaan01 <arsalaan0101@gmail.com>
2023-07-24 02:02:18 +0530

    course project angular

--- recipeapp/src/app/shopping-list/shopping-list.service.ts
+++ recipeapp/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
+import {Subject} from "rxjs";
 
 export class ShoppingListService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -23,5 +25,19 @@ export class ShoppingListService {
     // }
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  updateIngredient(index:number,newIngredients:Ingredient){
+    this.ingredients[index] = newIngredients;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
+  deleteIngredient(index){
+    this.ingredients.splice(index,1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
